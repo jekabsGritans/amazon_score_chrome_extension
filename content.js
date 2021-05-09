4ca827ab9795b56bf0c34ff1b59df34282aad747
Jekabs Gritans <jekabs.gritans@gmail.com>
2021-05-09 16:32:39 +0300

    Initial commit

---
+++ content.js
@@ -0,0 +1,11 @@
+var revs = document.getElementById("priceblock_ourprice");
+var main_num_reviews = parseInt(document.getElementById("acrCustomerReviewText").innerHTML.split(' ')[0].replace(/,/g, ''));
+var product_code = window.location.pathname.split('/')[3];
+var adjusted_product_code = 0
+
+//<span id="seller-rating-count-{iter}" class="a-size-small a-color-base">
+
+//<span>(468 ratings)<br>96% positive over last 12 months</span>
+
+
+//</span>
