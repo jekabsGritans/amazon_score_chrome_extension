354f339eeec009b539e474a23c7aeda6f5ef123c
Jekabs Gritans <jekabs.gritans@gmail.com>
2021-05-09 19:16:09 +0300

    optimized promise function, added popup

--- content.js
+++ content.js
@@ -1,31 +1,14 @@
-//var revs = document.getElementById("priceblock_ourprice");
 var main_num_reviews = parseInt(document.getElementById("acrCustomerReviewText").innerHTML.split(' ')[0].replace(/,/g, ''));
 var product_code = window.location.pathname.split('/')[3];
 const callApi = `https://reviewmeta.com/api/amazon/${product_code}`
 
-
-
 fetch(callApi)
-    .then(response => response.json())
-    .then(data => {
-      window.adj_rating = data.rating;
-      window.adj_num = data.count;
-      //got data, now run function(display score)
-      alert(adj_rating)
-    })
-    .catch(error => {
-        console.log('ERROR FETCHING THE API')
-        //error getting data, now display blank(greyed out) score
-    })
-
-
-
-
-
-//alert(callApi)
-//<span id="seller-rating-count-{iter}" class="a-size-small a-color-base">
-
-//<span>(468 ratings)<br>96% positive over last 12 months</span>
-
-
-//</span>
+  .then(response => response.json())
+  .then(data => {
+    alert(data.rating)
+    alert(data.count) //do stuff with API data
+  })
+  .catch(error => {
+    console.log('ERROR FETCHING THE API')
+    //error getting data, now display blank(greyed out) score
+  })
