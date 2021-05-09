7082ade2c13a0f230d05474a6cb0198c3e874502
Jekabs Gritans <jekabs.gritans@gmail.com>
2021-05-09 18:55:24 +0300

    request reviewmeta api resolved

--- content.js
+++ content.js
@@ -1,8 +1,28 @@
-var revs = document.getElementById("priceblock_ourprice");
+//var revs = document.getElementById("priceblock_ourprice");
 var main_num_reviews = parseInt(document.getElementById("acrCustomerReviewText").innerHTML.split(' ')[0].replace(/,/g, ''));
 var product_code = window.location.pathname.split('/')[3];
-var adjusted_product_code = 0
+const callApi = `https://reviewmeta.com/api/amazon/${product_code}`
 
+
+
+fetch(callApi)
+    .then(response => response.json())
+    .then(data => {
+      window.adj_rating = data.rating;
+      window.adj_num = data.count;
+      //got data, now run function(display score)
+      alert(adj_rating)
+    })
+    .catch(error => {
+        console.log('ERROR FETCHING THE API')
+        //error getting data, now display blank(greyed out) score
+    })
+
+
+
+
+
+//alert(callApi)
 //<span id="seller-rating-count-{iter}" class="a-size-small a-color-base">
 
 //<span>(468 ratings)<br>96% positive over last 12 months</span>
